7dd25f7f4119d1d073d5499243c4c074240a355c
alvadev <cristiandelacruz2709@gmail.com>
2024-01-24 23:20:34 -0600

    se creo la funcionalidad al navbar

--- src/ui/components/NavbarApp.jsx
+++ src/ui/components/NavbarApp.jsx
@@ -1,25 +1,34 @@
+import { Link, NavLink } from 'react-router-dom';
+
 export const NavbarApp = () => {
   return (
     <>
       <nav className="navbar navbar-expand-lg">
         <div className="container-fluid">
+
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
+
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <a className="navbar-brand" href="#">Navbar</a>
+            
+            <Link className="navbar-brand" to="/"> Navbar </Link>
+
             <ul className="navbar-nav d-flex justify-content-center align-items-center">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#"> Home </a>
+                <NavLink className={({isActive}) => `nav-link ${ isActive ? 'active' : ''}`} to="/home"> Home </NavLink>
               </li>
+
               <li className="nav-item">
-                <a className="nav-link" href="#"> About </a>
+                <NavLink className={({isActive}) => `nav-link ${ isActive ? 'active' : ''}`} to="/about"> About </NavLink>
               </li>
+
               <li className="nav-item">
-                <a className="nav-link" href="#"> Projects </a>
+                <NavLink className={({isActive}) => `nav-link ${ isActive ? 'active' : ''}`} to="/projects"> Projects </NavLink>
               </li>
-              <li className="nav-item dropdown">
-                <a className="nav-link" href="#"> Contact </a>
+
+              <li className="nav-item">
+                <NavLink className={({isActive}) => `nav-link ${ isActive ? 'active' : ''}`} to="/contact"> Contact </NavLink>
               </li>
             </ul>
           </div>
